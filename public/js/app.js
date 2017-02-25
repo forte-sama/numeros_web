4e34925d3906355b0aa5b76a0d4e5e782a582d08
Cesar A. Garcia <mrcesar9402@gmail.com>
2017-02-25 10:50:08 -0400

    estados de sesion iniciados

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,46 @@
-$(document).ready(function() {
-    $("div#container").load("test.html");
+
+function showLoggedOutUI() {
+    Materialize.toast("No hay na de sesion",5000);
+}
+
+function showLoggedInUI() {
+    Materialize.toast("Inicie sesion",5000);
+}
+
+// check authentication state
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+        showLoggedInUI();
+        // inicio sesion
+        // presentar animaciones para pantalla principal
+            // transicion para animacion de espera
+            // presentar animacion de espera
+            // transicion para animacion pantalla principal
+            // presentar pantalla principal
+        // habilitar todos los eventos
+    }
+    else {
+        // cerrar sesion
+        // presentar animaciones para pantalla login
+            // transicion para animacion de espera
+            // presentar animacion de espera
+            // transicion para animacion pantalla login
+            // presentar pantalla login
+        // deshabilitar todos los eventos
+        showLoggedOutUI();
+    }
+});
+
+$(document).ready(() => {
+    $("div#container").load("testz.html", () => {
+        // Materialize.toast(email.attr("class"), 10000);
+    });
+
+    // sign-in
+    firebase.auth().signOut();
+    // firebase.auth().signInWithEmailAndPassword("mrcesar9402@gmail.com","jolopero1");
+
+    var db = firebase.database();
+
+    db.ref("/_base_dia/piramide").set([5,4,3,2,1]);
 });
