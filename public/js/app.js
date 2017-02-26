0b142ca25c73624c10c94cd769d871a7e77da900
Cesar A. Garcia <mrcesar9402@gmail.com>
2017-02-26 12:08:02 -0400

    login terminado #tentativamente

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,54 @@
 
-function showLoggedOutUI() {
-    Materialize.toast("No hay na de sesion",5000);
-}
-
-function showLoggedInUI() {
-    Materialize.toast("Inicie sesion",5000);
-}
-
-// check authentication state
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser) {
-        showLoggedInUI();
-        // inicio sesion
-        // presentar animaciones para pantalla principal
-            // transicion para animacion de espera
-            // presentar animacion de espera
-            // transicion para animacion pantalla principal
-            // presentar pantalla principal
-        // habilitar todos los eventos
-    }
-    else {
-        // cerrar sesion
-        // presentar animaciones para pantalla login
-            // transicion para animacion de espera
-            // presentar animacion de espera
-            // transicion para animacion pantalla login
-            // presentar pantalla login
-        // deshabilitar todos los eventos
-        showLoggedOutUI();
-    }
-});
-
 $(document).ready(() => {
-    $("div#container").load("testz.html", () => {
-        // Materialize.toast(email.attr("class"), 10000);
-    });
+    const $container = $("div#container");
 
     // sign-in
-    firebase.auth().signOut();
+    // firebase.auth().signOut();
     // firebase.auth().signInWithEmailAndPassword("mrcesar9402@gmail.com","jolopero1");
 
-    var db = firebase.database();
+    // var db = firebase.database();
 
-    db.ref("/_base_dia/piramide").set([5,4,3,2,1]);
+    // animar de acuerdo a estado de autenticacion
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            // transicion para animacion de espera
+            $container.fadeToggle(250, function() {
+                // presentar animacion de espera
+                $container.load("/load.html", function() {
+                    // transicion para animacion pantalla principal
+                    $container.fadeToggle(250, function() {
+                        setTimeout(() => {
+                            // presentar animaciones para pantalla principal
+                            $container.fadeToggle(250, function() {
+                                // presentar pantalla principal
+                                $container.load("/main.html", function() {
+                                    $container.fadeToggle(250);
+                                });
+                            })
+                        }, 1500);
+                    });
+                });
+            });
+        }
+        else {
+            // transicion para animacion de espera
+            $container.fadeToggle(250, function() {
+                // presentar animacion de espera
+                $container.load("/load.html", function() {
+                    // transicion para animacion pantalla login
+                    $container.fadeToggle(250, function() {
+                        setTimeout(() => {
+                            // presentar animaciones para pantalla login
+                            $container.fadeToggle(250, function() {
+                                // presentar pantalla login
+                                $container.load("/login.html", function() {
+                                    $container.fadeToggle(250);
+                                });
+                            })
+                        }, 1500);
+                    });
+                });
+            });
+        }
+    });
 });
