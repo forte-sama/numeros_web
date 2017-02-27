0495b551bf99229539d0dfc51074ab9c9cbb27c0
Cesar A. Garcia <mrcesar9402@gmail.com>
2017-02-27 19:41:46 -0400

    numeros 3 dias se persisten en firebase realtime database

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,36 @@
 
+function changeContainerTo(container, targetPage) {
+    // transicion para animacion de espera
+    container.fadeToggle(250, function() {
+        // presentar animacion de espera
+        container.load("/load.html", function() {
+            // transicion para animacion pantalla principal
+            container.fadeToggle(250, function() {
+                setTimeout(() => {
+                    // presentar animaciones para pantalla principal
+                    container.fadeToggle(250, function() {
+                        // presentar pantalla principal
+                        container.load(targetPage, function() {
+                            container.fadeToggle(250);
+                        });
+                    })
+                }, 1500);
+            });
+        });
+    });
+}
+
 $(document).ready(() => {
     const $container = $("div#container");
-
-    // sign-in
-    // firebase.auth().signOut();
-    // firebase.auth().signInWithEmailAndPassword("mrcesar9402@gmail.com","jolopero1");
-
-    // var db = firebase.database();
+    const db = firebase.database();
 
     // animar de acuerdo a estado de autenticacion
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if(firebaseUser) {
-            // transicion para animacion de espera
-            $container.fadeToggle(250, function() {
-                // presentar animacion de espera
-                $container.load("/load.html", function() {
-                    // transicion para animacion pantalla principal
-                    $container.fadeToggle(250, function() {
-                        setTimeout(() => {
-                            // presentar animaciones para pantalla principal
-                            $container.fadeToggle(250, function() {
-                                // presentar pantalla principal
-                                $container.load("/main.html", function() {
-                                    $container.fadeToggle(250);
-                                });
-                            })
-                        }, 1500);
-                    });
-                });
-            });
+            changeContainerTo($container, "/main.html");
         }
         else {
-            // transicion para animacion de espera
-            $container.fadeToggle(250, function() {
-                // presentar animacion de espera
-                $container.load("/load.html", function() {
-                    // transicion para animacion pantalla login
-                    $container.fadeToggle(250, function() {
-                        setTimeout(() => {
-                            // presentar animaciones para pantalla login
-                            $container.fadeToggle(250, function() {
-                                // presentar pantalla login
-                                $container.load("/login.html", function() {
-                                    $container.fadeToggle(250);
-                                });
-                            })
-                        }, 1500);
-                    });
-                });
-            });
+            changeContainerTo($container, "/login.html");
         }
     });
 });
